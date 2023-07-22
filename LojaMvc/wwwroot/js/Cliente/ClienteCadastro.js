86b5621d344788ccef0baf6835043e3f80682ee4
Valdelir9356 <Valdelirsutiel@gmail.com>
2023-07-22 17:47:38 -0300

    Adicionado list e create do Cliente

---
+++ LojaMvc/wwwroot/js/Cliente/ClienteCadastro.js
@@ -0,0 +1,19 @@
+﻿document.getElementById("campo-cep").addEventListener("focusout", () => buscarDados());
+
+function buscarDados() {
+    let cep = document.getElementById("campo-cep").value.trim().replace("-", "");
+
+    if (cep.length != 8) {
+        return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => response.json())
+        .then((json) => {
+            document.getElementById("campo-cidade").value = json.localidade;
+            document.getElementById("campo-logradouro").value = json.logradouro;
+            document.getElementById("campo-bairro").value = json.bairro;
+            document.getElementById("campo-estado").value = json.uf;
+            document.getElementById("campo-numero").focus();
+        });
+}
---
+++ LojaRepositorios/Entidades/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaRepositorios.Entidades
+{
+    public class Cliente : ModeloBase
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
